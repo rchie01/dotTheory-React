ef2609ce3cbba10940835a4a82cd39db5ab8aa9d
Archie Yves Lim <archie.yves.lim@codeandtheory.com>
2022-11-16 18:05:13 +0800

    Added missing semi-colon

--- RCT-301/portals/src/App.js
+++ RCT-301/portals/src/App.js
@@ -9,7 +9,7 @@ import Modal from './components/Modal';
 class App extends Component {
 
   handleClick = () => {
-    console.log("I was clicked!")
+    console.log("I was clicked!");
   }
   
   render() {
